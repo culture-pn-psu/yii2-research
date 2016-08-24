7238bebf122f792f48721e26a7c12124e0741226
Madone <ahamad.jedu@gmail.com>
2016-08-24 16:05:08 +0700

    Add file & change nanespace

---
+++ Module.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace culturePnPsu\research;
+
+/**
+ * research module definition class
+ */
+class Module extends \yii\base\Module
+{
+    /**
+     * @inheritdoc
+     */
+    public $controllerNamespace = 'culturePnPsu\research\controllers';
+
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+        parent::init();
+
+        // custom initialization code goes here
+    }
+}
